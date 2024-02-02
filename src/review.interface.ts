0ad4bd146912c29d20eec6079599864fde82f651
Remus Watters <rwatters11@qub.ac.uk>
2024-02-02 12:27:37 +0000

    update services

--- src/review.interface.ts
+++ src/review.interface.ts
@@ -1,5 +1,5 @@
 export interface IReviewMessageDetails {
-  gigId?: string;
+  courseId?: string;
   reviewerId?: string;
   sellerId?: string;
   review?: string;
@@ -15,7 +15,7 @@ export interface IRatingTypes {
 
 export interface IReviewDocument {
   _id?: string;
-  gigId: string;
+  courseId: string;
   reviewerId: string;
   sellerId: string;
   review: string;
